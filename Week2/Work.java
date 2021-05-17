33014243a7e9660f73b076f346baf773972372bb
Muhammet Ali Ilgaz <73468385+ilgazali@users.noreply.github.com>
2021-05-17 12:04:16 +0300

    java_react_week_2

---
+++ Week2/Work.java
@@ -0,0 +1,12 @@
+package oopIntro;
+
+public class Work {
+
+    public static void programaDahilOl(){
+        System.out.println("Kayıt başarılı");
+    }
+
+    public static void bitirVeDevamEt(){
+        System.out.println("Geçiş başarılı");
+    }
+}
